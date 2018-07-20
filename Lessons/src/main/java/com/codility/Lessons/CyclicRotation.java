62aa45b632c8429d7c3def13b02480b5d7556844
BARIS <BARIS@DESKTOP-0JHOM5M>
2018-07-21 00:04:54 +0300

    Cyclic

---
+++ Lessons/src/main/java/com/codility/Lessons/CyclicRotation.java
@@ -0,0 +1,13 @@
+package com.codility.Lessons;
+
+public class CyclicRotation {
+	
+	public static int[] solution(int[] A, int K) {
+		int[] B = new int[A.length];
+		for (int i = 0; i < A.length; i++) {
+			B[(i+K)%A.length]=A[i];
+		}
+		return B;
+	}
+
+}
--- Lessons/src/test/java/com/codility/Lessons/TestCodility.java
+++ Lessons/src/test/java/com/codility/Lessons/TestCodility.java
@@ -11,7 +11,8 @@ public class TestCodility {
 	public void testFrog() {
 		Assert.assertSame("Wrong!", 3, FrogJmp.solution(10, 85, 30));
 		Assert.assertSame("Wrong!", 2, FrogJmp.solution(10, 70, 30));
-
+		Assert.assertArrayEquals(new int[]{9, 7, 6, 3, 8}, CyclicRotation.solution(new int[] {3, 8, 9, 7, 6}, 3));
+		
 	}
 	
 
