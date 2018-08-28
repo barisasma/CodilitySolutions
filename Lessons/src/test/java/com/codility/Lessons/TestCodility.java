5e72a30810ed7a21e21b9b9f01765bfd28ed0366
BARIS <BARIS@DESKTOP-0JHOM5M>
2018-08-28 21:55:18 +0300

    FrogRiverOne

--- Lessons/src/test/java/com/codility/Lessons/TestCodility.java
+++ Lessons/src/test/java/com/codility/Lessons/TestCodility.java
@@ -9,6 +9,7 @@ public class TestCodility {
 	
 	@Test
 	public void testFrog() {
+		
 		Assert.assertSame("Wrong!", 3, FrogJmp.solution(10, 85, 30));
 		Assert.assertSame("Wrong!", 2, FrogJmp.solution(10, 70, 30));
 		Assert.assertArrayEquals(new int[]{9, 7, 6, 3, 8}, CyclicRotation.solution(new int[] {3, 8, 9, 7, 6}, 3));
@@ -18,6 +19,8 @@ public class TestCodility {
 		Assert.assertEquals(1, TapeEquilibrium.solution(new int[] {3,1,2,4,3}));
 		Assert.assertEquals(0, PermCheck.solution(new int[] {3,1,2,4,12}));
 		Assert.assertEquals(1, PermCheck.solution(new int[] {1,2,3,4,5}));
+		Assert.assertEquals(4, FrogRiverOne.solution(4,new int[] {1,3,1,4,2,3,4,4}));
+		Assert.assertEquals(-1, FrogRiverOne.solution(5,new int[] {1,3,1,4,2,3,4,4}));
 		
 	}
 	
